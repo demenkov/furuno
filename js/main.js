8d0c3a31d29a786c60985f205baf00519ade38d6
Ivan Demenkov <ivan@demenkov.com>
2012-12-01 17:07:24 +0300

    Added monitor

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 //main function
 jQuery(document).ready(function($) {
-	jQuery(document).bind('keydown', 'f1', function (evt) { jQuery('#f1').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f2', function (evt) { jQuery('#f2').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f3', function (evt) { jQuery('#f3').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f4', function (evt) { jQuery('#f4').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f5', function (evt) { jQuery('#f5').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f6', function (evt) { jQuery('#f6').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f7', function (evt) { jQuery('#f7').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f8', function (evt) { jQuery('#f8').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f9', function (evt) { jQuery('#f9').dropdown('toggle'); return false;});
-	jQuery(document).bind('keydown', 'f10', function (evt) { jQuery('#f10').dropdown('toggle'); return false;});
-
-	jQuery(document).bind('keydown', '1', numberPressed);
-	jQuery(document).bind('keydown', '2', numberPressed);
-	jQuery(document).bind('keydown', '3', numberPressed);
-	jQuery(document).bind('keydown', '4', numberPressed);
-	jQuery(document).bind('keydown', '5', numberPressed);
-	jQuery(document).bind('keydown', '6', numberPressed);
-	jQuery(document).bind('keydown', '7', numberPressed);
-	jQuery(document).bind('keydown', '8', numberPressed);
-	jQuery(document).bind('keydown', '9', numberPressed);
-	jQuery(document).bind('keydown', '0', numberPressed);
+	//add events for functional buttons
+	$(document).bind('keydown', 'f1', function (evt) { $('#f1').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f2', function (evt) { $('#f2').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f3', function (evt) { $('#f3').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f4', function (evt) { $('#f4').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f5', function (evt) { $('#f5').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f6', function (evt) { $('#f6').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f7', function (evt) { $('#f7').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f8', function (evt) { $('#f8').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f9', function (evt) { $('#f9').dropdown('toggle'); return false;});
+	$(document).bind('keydown', 'f10', function (evt) { $('#f10').dropdown('toggle'); return false;});
+	//add events for numeric buttons
+	$(document).bind('keydown', '1', numberPressed);
+	$(document).bind('keydown', '2', numberPressed);
+	$(document).bind('keydown', '3', numberPressed);
+	$(document).bind('keydown', '4', numberPressed);
+	$(document).bind('keydown', '5', numberPressed);
+	$(document).bind('keydown', '6', numberPressed);
+	$(document).bind('keydown', '7', numberPressed);
+	$(document).bind('keydown', '8', numberPressed);
+	$(document).bind('keydown', '9', numberPressed);
+	$(document).bind('keydown', '0', numberPressed);
 
 	//process numeric buttons
 	function numberPressed(e) {
@@ -28,4 +29,23 @@ jQuery(document).ready(function($) {
 		$($('#menu li.dropdown.open ul li a')[String.fromCharCode( e.which ).toLowerCase() - 1]).focus();
 		return false;
 	}
+
+	var felcom = {
+		lat		:0,
+		lon		:0,
+		date	:new Date(),
+		init	: function() {
+			var self = this;
+			//set datetime counter
+			felcom.time($('#status span.date'));
+			setInterval(function(){self.time($('#status span.date'))}, 1000);
+		},
+		time	: function(el) {
+			$(el).html(
+				this.date.toString("yy-mm-dd HH:mm" + ' (UTC)')
+			);
+		}
+	};
+
+	felcom.init();
 });
